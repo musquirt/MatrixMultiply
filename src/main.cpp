a232b736386b5eb20b6eeabc4458b4373e2de850
Larry Price <larry.rick.price@gmail.com>
2011-11-23 22:38:55 -0500

    well, some things work and some don't. not sure where to go from here

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,92 @@
 #include <unistd.h>
 #include <cstdlib>
 #include "Matrix.cpp"
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
-#define ROWS_1 50
-#define COLS_1 500
+#define ROWS_1 3
+#define COLS_1 4
 #define ROWS_2 COLS_1
-#define COLS_2 500
-#define NUM_THREADS  10
+#define COLS_2 3
+#define NUM_THREADS  4
 
 void fillIdent(double *a, int n, double v=1);
-void fillIncr(double *a, int n);
+void fillIncr(double *a, int n, int startVal=0);
 void fillRand(double *a, int n);
 void* multiply(void* arg);
 void printMatrix(double* , int , int );
 void interpretTime(timeval , timeval );
+bool getCommandLineOptions(int argc, char** argv, int &rows_a, int &rows_b, int &cols_a, int &cols_b, int &matrixType, int &num, int &num_threads) {
+	for (int i=1; i<argc; i++) {
+		string arg = argv[i];
+		if (arg == "-a") {
+			if (i+1 < argc) {
+				i++;
+				rows_a = atoi(argv[i]);
+				if (i+1 < argc && argv[i+1][0] != '-') {
+					i++;
+					cols_a = atoi(argv[i]);
+				} else {
+					cols_a = rows_a;
+				}
+			} else {
+				cout << "Expected integer(s) number after -a" << endl;
+				return false;
+			}
+		} else if (arg == "-b") {
+			if (i+1 < argc) {
+				i++;
+				rows_b = atoi(argv[i]);
+				if (i+1 < argc && argv[i+1][0] != '-') {
+					i++;
+					cols_b = atoi(argv[i]);
+				} else {
+					cols_b = rows_b;
+				}
+			} else {
+				cout << "Expected integer(s) number after -b" << endl;
+				return false;
+			}
+		} else if (arg == "-rand") {
+			matrixType = 1;
+		} else if (arg == "-ident") {
+			matrixType = 0;
+			if ((i+1) < argc) {
+				i++;
+				num = atoi(argv[i]);
+			} else {
+				cout << "Expected integer number after -ident" << endl;
+				return false;
+			}
+		} else if (arg == "-incr") {
+			matrixType = 2;
+			if ((i+1) < argc) {
+				i++;
+				num = atoi(argv[i]);
+			} else {
+				cout << "Expected integer number after -incr" << endl;
+				return false;
+			}
+		} else if (arg == "-h" || arg == "-help") {
+			
+			return false;
+		} else if (arg == "-t") {
+			if (i+1<argc) {
+				i++;
+				num_threads = atoi(argv[i]);
+			} else {
+				cout << "Expected integer number after -t" << endl;
+			}
+		} else {
+			cout << "Usage: " << argv[0] << " [-a rows_a [cols_a]] [-b rows_b [cols_b]] [-t num_threads] [-rand|-ident num|-incr start]" << endl;
+			return false;
+		}
+	}
+	
+	return true;
+}
 
 int main(int argc, char** argv) {
 	int rows_a = ROWS_1;
@@ -27,14 +98,19 @@ int main(int argc, char** argv) {
 	int cols_a = COLS_1;
 	int cols_b = COLS_2;
 	int n_threads = NUM_THREADS;
+	int matrixType = -1;
+	int num = 0;
 	
 	// get command  line opts
+	if (!getCommandLineOptions(argc, argv, rows_a, rows_b, cols_a, cols_b, matrixType, num, n_threads)) {
+		return -1;
+	}
 	
 	if (cols_a != rows_b) {
 		cout << "Number columns in first must equal number rows in second!"
 		     << endl;
 		return -1;
-	} else if (rows_a < n_threads) {
+	} else if (n_threads > rows_a) {
 		n_threads = rows_a;
 	}
 	
@@ -42,23 +118,39 @@ int main(int argc, char** argv) {
 	double *b = (double *)malloc(rows_b*cols_b * sizeof(double));
 	double *c = (double *)malloc(rows_a*cols_b * sizeof(double));
 	
-	fillIncr(a, rows_a*cols_a);
-	fillIncr(b, rows_b*cols_b);
-	fillIdent(c, rows_a*cols_b, 0);
+	switch(matrixType) {
+		case 0:
+			fillIdent(a, rows_a*cols_a, num);
+			fillIdent(b, rows_b*cols_b, num);
+			break;
+		case 1:
+			fillRand(a, rows_a*cols_a);
+			fillRand(b, rows_b*cols_b);
+			break;
+		case 2:
+			fillIncr(a, rows_a*cols_a, num);
+			fillIncr(b, rows_b*cols_b, num);
+			break;
+		default:
+			fillIdent(a, rows_a*cols_a, 1);
+			fillIdent(b, rows_b*cols_b, 1);
+			break;
+	}
 	
 	pthread_t threads[n_threads];
 	
 	Matrix::setA(a);
 	Matrix::setB(b);
 	Matrix::setC(c);
-	Matrix::n_by_t = n_threads * cols_a; // Fixed
+	Matrix::n_by_t = n_threads * cols_a;
+	Matrix::num_threads = n_threads;
 	
 	list< Matrix> m;
 	
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
 	for (int i=0; i<n_threads; i++) {
-		Matrix p(i, n_threads, rows_a, cols_a, cols_b);
+		Matrix p(i, rows_a, cols_a, cols_b);
 		m.push_back(p);
 		pthread_create(&(threads[i]), NULL, multiply, (void*) &m.back());
 	}
@@ -68,13 +160,33 @@ int main(int argc, char** argv) {
 	}
 	gettimeofday(&end, NULL);
 	
-	interpretTime(start, end);
+	//interpretTime(start, end);
 	
-	#if 0
+	#if 1
+	//printMatrix(a, rows_a, cols_a);
+	//cout << "multiplied by" << endl;
+	//printMatrix(b, rows_b, cols_b);
+	//cout << "equals" << endl;
 	printMatrix(c, rows_a, cols_b);
 	#endif
 	
 	return 0;
+}
+
+void* multiply(void* arg) {
+	Matrix m = *((Matrix*) arg);
+	for (int i=m.tid*m.n; i<m.m*m.n; i+=Matrix::n_by_t) {
+		for (int j=0; j<m.q; j++) {
+			double sum = 0;
+			for (int k=0; k<m.n; k++) {
+				sum += Matrix::a[i+k]*Matrix::b[j+k*m.q];
+			}
+			stringstream out;
+			out << i/Matrix::num_threads*m.q + j << " " << m.m*m.q << endl;
+			cout << out.str();
+			Matrix::c[i/Matrix::num_threads*m.q + j] = sum;
+		}
+	} 
 }
 
 void interpretTime(timeval start, timeval end) {
@@ -103,8 +215,8 @@ void fillIdent(double *a, int n, double v) {
 	return;
 }
 
-void fillIncr(double *a, int n) {
-	for (int i=0; i<n; i++) {
+void fillIncr(double *a, int n, int startVal) {
+	for (int i=startVal; i<n; i++) {
 		a[i] = i;
 	}
 	return;
@@ -117,23 +229,5 @@ void fillRand(double *a, int n) {
 		a[i] = rand();
 	}
 	return;
-}
-
-void* multiply(void* arg) {
-	Matrix m = *((Matrix*) arg);
-	for (int i=m.tid*m.n; i<m.m*m.n; i+=Matrix::n_by_t) {
-		for (int j=0; j<m.q; j++) {
-			double sum = 0;
-			for (int k=0; k<m.n; k++) {
-				sum += Matrix::a[i+k]*Matrix::b[j+k*m.q]; // Fixed
-				//cout << i <<", " << j << ", " << k << ", " << k*m.q << ": " << Matrix::a[i+k] << " * " << Matrix::b[j+k*m.q] << endl;
-			}
-			Matrix::c[i/Matrix::n_by_t+1+j] = sum; // Fixed
-			//cout << sum << " ";
-			//cout << endl;
-		}
-		//cout << endl;
-	} 
-	//cout << endl;
 }
 
